18cff6b6231fcdfb716df0cf2907c0deb8a870d2
Amin Bahiraei <mr.bahiraee@gmail.com>
2015-04-12 10:32:24 +0430

    first Commit

---
+++ app/src/main/java/ir/bigoloo/android/pishshomare/MainActivity.java
@@ -0,0 +1,64 @@
+package ir.bigoloo.android.pishshomare;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new PishshomareWebClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl(getString(R.string.pishshomare_url));
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+
+            webView.goBack();
+
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    private class PishshomareWebClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+            super.onPageFinished(view, url);
+        }
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+
+    }
+
+
+  }
